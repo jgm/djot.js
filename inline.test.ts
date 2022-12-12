67f3afb1fd88319fd870ae67fe37375f396e12b1
John MacFarlane <jgm@berkeley.edu>
2022-12-11 16:56:49 -0800

    Add (failing) escapes test.

--- inline.test.ts
+++ inline.test.ts
@@ -10,6 +10,7 @@ describe("InlineParser", () => {
       { annot: "str", startpos: 8, endpos: 10 }
     ]);
   });
+
   it("parses verbatim", () => {
     let parser = new InlineParser("x ``` hello ``there ``` x", () => {});
     //                             0123456789012345678901234
@@ -21,5 +22,21 @@ describe("InlineParser", () => {
       { annot: "-verbatim", startpos: 20, endpos: 22 },
       { annot: "str", startpos: 23, endpos: 24 }
     ]);
-  })
+  });
+
+  it("parses escapes", () => {
+    let parser = new InlineParser('\\"\\*\\ \\a \\n', () => {});
+    parser.feed(0,11);
+    expect(parser.getMatches()).toStrictEqual([
+      { annot: "escape", startpos: 0, endpos: 0 },
+      { annot: "str", startpos: 1, endpos: 1 },
+      { annot: "escape", startpos: 2, endpos: 2 },
+      { annot: "str", startpos: 3, endpos: 3 },
+      { annot: "escape", startpos: 4, endpos: 4 },
+      { annot: "nbsp", startpos: 5, endpos: 5 },
+      { annot: "str", startpos: 6, endpos: 7 },
+      { annot: "escape", startpos: 9, endpos: 9 },
+      { annot: "hardbreak", startpos: 10, endpos: 10 }
+    ]);
+  });
 })
