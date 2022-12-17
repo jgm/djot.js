2c0dbf93078afb5c48ca52767c3158f4bd11a109
John MacFarlane <jgm@berkeley.edu>
2022-12-17 12:24:53 -0800

    Shell of ast.

--- ast.ts
+++ ast.ts
@@ -1,7 +1,67 @@
 import { Event } from "./event.js";
-import { AttributeParser } from "./attributes.js";
-import { pattern, find, boundedFind } from "./find.js";
-import { InlineParser } from "./inline.js";
-import { Parser } from "./block.js";
+import { EventParser } from "./block.js";
+
+// Types for the AST
+
+type Block = Para
+//           | BlockQuote
+//           | ThematicBreak
+//           | List
+//           | Table
+           | Heading ;
+
+type Inline = Str
+//            | SoftBreak
+//            | HardBreak
+//            | Emph
+//            | Strong
+//            | Link
+//            | Image
+//            | Span
+//            | Mark
+//            | Insert
+//            | Delete
+//            | Strikeout
+            | Emoji ;
+
+interface Doc {
+  tag: "doc";
+  children: Block[];
+  references: Record<string, Reference>;
+  footnotes: Record<string, Footnote>;
+}
+
+interface Para {
+  tag: "para";
+  children: Inline[];
+}
+
+interface Heading {
+  tag: "heading";
+  children: Inline[];
+}
+
+
+interface Str {
+  tag: "str";
+  text: string;
+}
+
+interface Emoji {
+  tag: "emoji";
+  text: string;
+}
+
+interface Reference {
+
+}
+
+interface Footnote {
+
+}
+
+class Parser {
+
+}
 
 export { Parser }
