c43057048de977626d6259a6fcbc9bad5b880102
John MacFarlane <jgm@berkeley.edu>
2022-12-19 12:02:54 -0800

    Add unit test.

--- ast.test.ts
+++ ast.test.ts
@@ -3,6 +3,44 @@ import { parse, ParseOptions, Doc } from "./ast.js";
 const ignoreWarnings = () => { /* do nothing */ };
 
 describe("Parser", () => {
-  it("does nothing", () => {
+  it("parses paragraphs", () => {
+    let ast = parse("hi there\nfriend\n\nnew para", {});
+    expect(ast).toEqual(
+{
+  "tag": "doc",
+  "references": {},
+  "footnotes": {},
+  "children": [
+    {
+      "tag": "para",
+      "children": [
+        {
+          "tag": "str",
+          "text": "hi there"
+        },
+        {
+          "tag": "softbreak"
+        },
+        {
+          "tag": "str",
+          "text": "friend"
+        }
+      ]
+    },
+    {
+      "tag": "para",
+      "children": [
+        {
+          "tag": "str",
+          "text": "new para"
+        }
+      ]
+    }
+  ]
+}
+    );
   });
+
+
+
 });
