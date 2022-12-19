4cc9e24e95fcfe81ed6379980fd617551ffed840
John MacFarlane <jgm@berkeley.edu>
2022-12-18 22:26:46 -0800

    Do a stack trace on error (run.mjs).

--- run.mjs
+++ run.mjs
@@ -9,23 +9,31 @@ const warn = function(msg, pos) {
 }
 
 let start = true;
-for (const event of new EventParser(input, warn)) {
-  let pref;
-  if (start) {
-    pref = "[";
-    start = false;
-  } else {
-    pref = ",";
+
+try {
+  for (const event of new EventParser(input, warn)) {
+    let pref;
+    if (start) {
+      pref = "[";
+      start = false;
+    } else {
+      pref = ",";
+    }
+    process.stdout.write(pref + '["' + event.annot + '",' + event.startpos +
+                           ',' + event.endpos + ']\n');
   }
-  process.stdout.write(pref + '["' + event.annot + '",' + event.startpos +
-                         ',' + event.endpos + ']\n');
+  console.log("]");
+
+  console.time("parse AST");
+  let ast = parse(input, {});
+  console.timeEnd("parse AST");
+
+  process.stdout.write(JSON.stringify(ast, null, 2));
+  process.stdout.write("\n");
+} catch(err) {
+    console.log(err + "\n");
+    if (err.stack) {
+      console.log(err.stack.split("\n"));
+    }
+    process.exit(1);
 }
-console.log("]");
-
-console.time("parse AST");
-let ast = parse(input, {});
-console.timeEnd("parse AST");
-
-process.stdout.write(JSON.stringify(ast, null, 2));
-process.stdout.write("\n");
-
