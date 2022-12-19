365493a12388d269148d8a4cfcddb18185192cd7
John MacFarlane <jgm@berkeley.edu>
2022-12-19 10:39:37 -0800

    Improve cli.

--- run.mjs
+++ run.mjs
@@ -2,11 +2,47 @@ import { EventParser } from "./block.js";
 import { parse } from "./ast.js";
 import fs from "fs";
 
-const input = fs.readFileSync("/dev/stdin", "utf8");
-
 const warn = function(msg, pos) {
-//  process.stderr.write(msg + (pos ? " at " + pos : "") + "\n");
+  process.stderr.write(msg + (pos ? " at " + pos : "") + "\n");
 }
+
+let options = {sourcePositions: false, warn: warn};
+let usage = `node ./run.mjs [OPTIONS] FILE*
+Options:
+--sourcepos,-p       Include source positions
+--quiet,-q           Suppress warnings
+--help,-h            This usage message
+`;
+let files = [];
+
+for (let i=2; i < process.argv.length; i++) {
+  let arg = process.argv[i];
+  switch (arg) {
+    case "--sourcepos":
+    case "-p":
+      options.sourcePositions = true;
+      break;
+    case "--quiet":
+    case "-q":
+      options.warn = (msg, pos) => {};
+      break;
+    case "--help":
+    case "-h":
+      process.stdout.write(usage);
+      process.exit(0);
+      break;
+    default:
+      files.push(arg);
+  }
+}
+
+let input = "";
+if (files.length === 0) {
+  files = ["/dev/stdin"];
+}
+files.forEach(file => {
+  input = input + fs.readFileSync(file, "utf8");
+});
 
 let start = true;
 
@@ -37,3 +73,5 @@ try {
     }
     process.exit(1);
 }
+
+process.exit(0);
