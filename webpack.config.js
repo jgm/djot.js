a5d23a943a7f4b591bb927aadf5d94522758dfdd
John MacFarlane <jgm@berkeley.edu>
2022-12-22 10:44:06 -0800

    Update webpack config to create umd output.
    This should work in the browser (script tag) and in node with require.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './index.ts',
   module: {
     rules: [
@@ -18,5 +18,10 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    globalObject: 'this',
+    library: {
+      name: 'djot',
+      type: 'umd'
+    }
   },
 };
