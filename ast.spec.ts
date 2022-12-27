5567abf481da85c01c3b11c1d5f7558cbed5cb3d
John MacFarlane <jgm@berkeley.edu>
2022-12-27 12:56:18 -0800

    Omit sourcepos on doc in ast spec test.

--- ast.spec.ts
+++ ast.spec.ts
@@ -132,19 +132,7 @@ describe("Parser", () => {
         }
       }
     }
-  ],
-  "pos": {
-    "start": {
-      "line": 0,
-      "col": 0,
-      "offset": 0
-    },
-    "end": {
-      "line": 1,
-      "col": 20,
-      "offset": 19
-    }
-  }
+  ]
 }
     );
   });
