a895b14c779ec7a8b40af6769800c9c479e33c3c
John MacFarlane <jgm@berkeley.edu>
2023-01-02 12:18:55 -0800

    Add table test for djot renderer.

--- src/djot-renderer.spec.ts
+++ src/djot-renderer.spec.ts
@@ -78,5 +78,163 @@ etc.
 `);
    });
 
+  it("does tables", () => {
+    const cicero3 : Doc = mkdoc([
+    {
+      "tag": "table",
+      "children": [
+        {
+          "tag": "row",
+          "children": [
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "a"
+                }
+              ],
+              "head": true,
+              "align": "left"
+            },
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "b"
+                }
+              ],
+              "head": true,
+              "align": "right"
+            }
+          ],
+          "head": true
+        },
+        {
+          "tag": "row",
+          "children": [
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "c"
+                }
+              ],
+              "head": false,
+              "align": "left"
+            },
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "d"
+                }
+              ],
+              "head": false,
+              "align": "right"
+            }
+          ],
+          "head": false
+        },
+        {
+          "tag": "row",
+          "children": [
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "cc"
+                }
+              ],
+              "head": true,
+              "align": "right"
+            },
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "dd"
+                }
+              ],
+              "head": true,
+              "align": "center"
+            }
+          ],
+          "head": true
+        },
+        {
+          "tag": "row",
+          "children": [
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "e"
+                }
+              ],
+              "head": false,
+              "align": "right"
+            },
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "f"
+                }
+              ],
+              "head": false,
+              "align": "center"
+            }
+          ],
+          "head": false
+        },
+        {
+          "tag": "row",
+          "children": [
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "g"
+                }
+              ],
+              "head": false,
+              "align": "right"
+            },
+            {
+              "tag": "cell",
+              "children": [
+                {
+                  "tag": "str",
+                  "text": "h"
+                }
+              ],
+              "head": false,
+              "align": "center"
+            }
+          ],
+          "head": false
+        }
+      ]
+    }]);
+    expect(new DjotRenderer(cicero3, 30).render()).toStrictEqual(
+`|a|b|
+|:--|--:|
+|c|d|
+|cc|dd|
+|--:|:-:|
+|e|f|
+|g|h|
+`);
+   });
+
 });
 
