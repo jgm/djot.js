3ff20f13a2e8395b5203a37f4ec68f417d3a3c1e
John MacFarlane <jgm@berkeley.edu>
2024-01-30 19:32:59 -0800

    Remove most of the benchmark files.
    These were hastily converted from commonmark.js benchmarks,
    and in many cases didn't even test the same thing due to escaping.

--- bench/bench.js
+++ bench/bench.js
@@ -13,25 +13,6 @@ const djotrenderer = (ast) => djot.renderHTML(ast, {warn: () => {}});
 const convert = (contents) => djot.renderHTML(djot.parse(contents, options), options);
 
 const files = [
-  "block-bq-flat.dj",
-  "block-bq-nested.dj",
-  "block-code.dj",
-  "block-fences.dj",
-  "block-heading.dj",
-  "block-hr.dj",
-  "block-list-flat.dj",
-  "block-list-nested.dj",
-  "block-ref-flat.dj",
-  "block-ref-nested.dj",
-  "inline-autolink.dj",
-  "inline-backticks.dj",
-  "inline-em-flat.dj",
-  "inline-em-nested.dj",
-  "inline-em-worst.dj",
-  "inline-escape.dj",
-  "inline-links-flat.dj",
-  "inline-links-nested.dj",
-  "lorem1.dj",
   "readme.dj" ];
 
 var pattern = process.argv[2];
